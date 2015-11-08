03c63367a89766e4e07deeab63a66c23c9751ab6
Wolfgang Marcos <wolfgang.tw@gmail.com>
2015-11-07 23:28:20 -0400

    Adiciona suporte inicial a sorteios

--- src/br/nom/marcos/wolfgang/desktop/bingo/controller/br/nom/marcos/wolfgang/desktop/bingo/view/FormPrincipal.java
+++ src/br/nom/marcos/wolfgang/desktop/bingo/controller/br/nom/marcos/wolfgang/desktop/bingo/view/FormPrincipal.java
@@ -1,6 +1,12 @@
 package br.nom.marcos.wolfgang.desktop.bingo.controller.br.nom.marcos.wolfgang.desktop.bingo.view;
 
+import br.nom.marcos.wolfgang.desktop.bingo.controller.Sorteador;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.LinkedList;
 
 /**
  * Created by Wolfgang on 07/11/2015.
@@ -32,8 +38,6 @@ public class FormPrincipal {
     private JLabel lblNum13;
     private JLabel lblNum14;
     private JLabel lblNum15;
-    private JPanel panPrimeiraLetra;
-    private JPanel panSegundaLetra;
     private JPanel panNum16_22;
     private JPanel panNum23_29;
     private JLabel lblNum30;
@@ -52,7 +56,6 @@ public class FormPrincipal {
     private JLabel lblNum28;
     private JLabel lblNum29;
     private JLabel lblNum31;
-    private JPanel panTerceiraLetra;
     private JLabel lblNum32;
     private JLabel lblNum33;
     private JLabel lblNum34;
@@ -69,7 +72,6 @@ public class FormPrincipal {
     private JLabel lblNum43;
     private JLabel lblNum44;
     private JLabel lblNum45;
-    private JPanel panQuartaLetra;
     private JPanel panNum46_52;
     private JPanel panNum53_59;
     private JLabel lblNum46;
@@ -87,7 +89,6 @@ public class FormPrincipal {
     private JLabel lblNum58;
     private JLabel lblNum59;
     private JLabel lblNum60;
-    private JPanel panQuintaLetra;
     private JPanel panNum61_67;
     private JPanel panNum68_74;
     private JLabel lblNum75;
@@ -106,12 +107,188 @@ public class FormPrincipal {
     private JLabel lblNum72;
     private JLabel lblNum73;
     private JLabel lblNum74;
+    private JLabel lblPrimeiraLetra;
+    private JPanel panPrimeiraLetra;
+    private JPanel panSegundaLetra;
+    private JLabel lblSegundaLetra;
+    private JPanel panTerceiraLetra;
+    private JLabel lblTerceiraLetra;
+    private JPanel panQuartaLetra;
+    private JLabel lblQuartaLetra;
+    private JPanel panQuintaLetra;
+    private JLabel lblQuintaLetra;
+
+    private LinkedList<JLabel> lstNumeros;
+    private Sorteador sorteador;
+    private String[] letras = {"B", "I", "N", "G", "O"};
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Bingo");
         frame.setContentPane(new FormPrincipal().panPrincipal);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setMinimumSize(new Dimension(900, 500));
         frame.pack();
         frame.setVisible(true);
+    }
+
+    public FormPrincipal() {
+        initListaNumeros();
+        initComponentes();
+        initSorteio();
+    }
+
+    private void initSorteio(){
+        sorteador = new Sorteador();
+        initInterface();
+    }
+
+    private void initInterface(){
+        lblLetraSorteada.setText("Boa");
+        lblNumeroSorteado.setText("Sorte");
+
+        initLetras();
+
+        for(JLabel lblNumero : lstNumeros)
+            marcarNumeroNaoSorteado(lblNumero);
+    }
+
+    private void initLetras(){
+        lblPrimeiraLetra.setText(letras[0]);
+        lblSegundaLetra.setText(letras[1]);
+        lblTerceiraLetra.setText(letras[2]);
+        lblQuartaLetra.setText(letras[3]);
+        lblQuintaLetra.setText(letras[4]);
+    }
+
+    private void initListaNumeros(){
+        lstNumeros = new LinkedList<JLabel>();
+        lstNumeros.add(lblNum01);
+        lstNumeros.add(lblNum02);
+        lstNumeros.add(lblNum03);
+        lstNumeros.add(lblNum04);
+        lstNumeros.add(lblNum05);
+        lstNumeros.add(lblNum06);
+        lstNumeros.add(lblNum07);
+        lstNumeros.add(lblNum08);
+        lstNumeros.add(lblNum09);
+        lstNumeros.add(lblNum10);
+        lstNumeros.add(lblNum11);
+        lstNumeros.add(lblNum12);
+        lstNumeros.add(lblNum13);
+        lstNumeros.add(lblNum14);
+        lstNumeros.add(lblNum15);
+        lstNumeros.add(lblNum16);
+        lstNumeros.add(lblNum17);
+        lstNumeros.add(lblNum18);
+        lstNumeros.add(lblNum19);
+        lstNumeros.add(lblNum20);
+        lstNumeros.add(lblNum21);
+        lstNumeros.add(lblNum22);
+        lstNumeros.add(lblNum23);
+        lstNumeros.add(lblNum24);
+        lstNumeros.add(lblNum25);
+        lstNumeros.add(lblNum26);
+        lstNumeros.add(lblNum27);
+        lstNumeros.add(lblNum28);
+        lstNumeros.add(lblNum29);
+        lstNumeros.add(lblNum30);
+        lstNumeros.add(lblNum31);
+        lstNumeros.add(lblNum32);
+        lstNumeros.add(lblNum33);
+        lstNumeros.add(lblNum34);
+        lstNumeros.add(lblNum35);
+        lstNumeros.add(lblNum36);
+        lstNumeros.add(lblNum37);
+        lstNumeros.add(lblNum38);
+        lstNumeros.add(lblNum39);
+        lstNumeros.add(lblNum40);
+        lstNumeros.add(lblNum41);
+        lstNumeros.add(lblNum42);
+        lstNumeros.add(lblNum43);
+        lstNumeros.add(lblNum44);
+        lstNumeros.add(lblNum45);
+        lstNumeros.add(lblNum46);
+        lstNumeros.add(lblNum47);
+        lstNumeros.add(lblNum48);
+        lstNumeros.add(lblNum49);
+        lstNumeros.add(lblNum50);
+        lstNumeros.add(lblNum51);
+        lstNumeros.add(lblNum52);
+        lstNumeros.add(lblNum53);
+        lstNumeros.add(lblNum54);
+        lstNumeros.add(lblNum55);
+        lstNumeros.add(lblNum56);
+        lstNumeros.add(lblNum57);
+        lstNumeros.add(lblNum58);
+        lstNumeros.add(lblNum59);
+        lstNumeros.add(lblNum60);
+        lstNumeros.add(lblNum61);
+        lstNumeros.add(lblNum62);
+        lstNumeros.add(lblNum63);
+        lstNumeros.add(lblNum64);
+        lstNumeros.add(lblNum65);
+        lstNumeros.add(lblNum66);
+        lstNumeros.add(lblNum67);
+        lstNumeros.add(lblNum68);
+        lstNumeros.add(lblNum69);
+        lstNumeros.add(lblNum70);
+        lstNumeros.add(lblNum71);
+        lstNumeros.add(lblNum72);
+        lstNumeros.add(lblNum73);
+        lstNumeros.add(lblNum74);
+        lstNumeros.add(lblNum75);
+    }
+
+    private void sortearNumero(){
+        int numeroSorteado = sorteador.sortearNumero();
+        if(numeroSorteado == 0) {
+            JOptionPane.showMessageDialog(panPrincipal, "Não há mais números a serem sorteados!", "Temos um vencedor!", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        marcarNumeroSorteado(lstNumeros.get(numeroSorteado - 1));
+        lblLetraSorteada.setText(calcularLetraSorteada(numeroSorteado));
+        lblNumeroSorteado.setText(String.valueOf(numeroSorteado));
+    }
+
+    private String calcularLetraSorteada(int numeroSorteado){
+        if(numeroSorteado <= 15)
+            return letras[0];
+        else if(numeroSorteado <= 30)
+            return letras[1];
+        else if(numeroSorteado <= 45)
+            return letras[2];
+        else if(numeroSorteado <= 60)
+            return letras[3];
+        else
+            return letras[4];
+    }
+
+    private void marcarNumeroSorteado(JLabel lblNumero){
+        pintarNumero(lblNumero, Color.BLACK);
+    }
+
+    private void marcarNumeroNaoSorteado(JLabel lblNumero){
+        pintarNumero(lblNumero, Color.LIGHT_GRAY);
+    }
+
+    private void pintarNumero(JLabel lblNumero, Color color){
+        lblNumero.setForeground(color);
+    }
+
+    public void setLetras(String[] letras){
+        if(letras.length == 5) {
+            this.letras = letras;
+            initLetras();
+        }
+    }
+
+    private void initComponentes(){
+        btnSortear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sortearNumero();
+            }
+        });
     }
 }
